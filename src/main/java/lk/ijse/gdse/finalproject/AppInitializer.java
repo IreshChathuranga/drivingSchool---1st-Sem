bb252657d904791ec42f254eca3ee0b3ffdefdfc
IreshChathuranga <ireshchathuranga63309.com>
2024-10-28 09:06:15 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse/finalproject/AppInitializer.java
@@ -0,0 +1,35 @@
+package lk.ijse.gdse.finalproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource());
+        Scene scene = new Scene(load);
+        stage.setScene(scene);
+        stage.setTitle("");
+
+
+//        Parent load = FXMLLoader.load(getClass().getResource("/view/LogIn.fxml"));
+//        Scene scene = new Scene(load);
+//        stage.setScene(scene);
+//        stage.setTitle("learn Drive");
+//        Image image = new Image(getClass().getResourceAsStream("/images/pexels-taras-makarenko-188506-593172.jpg"));
+//        stage.getIcons().add(image);
+//        stage.show();
+
+    }
+}
